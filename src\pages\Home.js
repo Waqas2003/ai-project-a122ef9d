c6f23d061de537e0c41c60f17c46b3c09f6ff905
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-03 21:10:29 +0500

    Add src\pages\Home.js

---
+++ src\pages\Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Welcome to Cosmetics Website</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
